f0d682a008e7b1f72128e00c1c4559b1e534f878
Nabin Budhathoki <tusharbcbudhathoki@gmail.com>
2025-06-30 23:40:52 +0545

    previous state for product handle in backend

--- server/controllers/admin/product-controller.js
+++ server/controllers/admin/product-controller.js
@@ -14,19 +14,14 @@ const handleImageUpload = async (req, res) => {
       let mimetype = file.mimetype;
       console.log("mimetype before converting", mimetype);
       // check for heic and convert to jpeg
-      try {
-        console.log("Starting HEIC conversion...");
+      if (mimetype == "image/heic") {
         const outputBuffer = await heicConvert({
           buffer,
           format: "JPEG",
           quality: 1,
         });
-        console.log("HEIC conversion successful");
         buffer = outputBuffer;
         mimetype = "image/jpeg";
-      } catch (err) {
-        console.error("HEIC conversion failed:", err);
-        throw err; // or handle gracefully
       }
       console.log("mimetype after converting", mimetype);
       const b64 = Buffer.from(buffer).toString("base64");
